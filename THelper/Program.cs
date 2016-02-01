c5eb472ccb816a0e537befcb3350106e2a763fd1
Andrey Kozhevnikov <freazeek@mail.ru>
2016-02-01 10:36:15 +0300

    Csproj set specific version

--- THelper/Program.cs
+++ THelper/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace THelper {
     class Program {
@@ -27,16 +29,14 @@ namespace THelper {
                     var proc = Process.Start(value, argsforWR);
                     proc.WaitForExit();
                     string path = string.Empty;
-                    if (GetFile(dirInfo, out path)) {
+                    string cspath = string.Empty;
+                    if (GetFile(dirInfo, out path, out cspath)) {
+                        FixCsprojSpecificVersion(cspath);
                         Process.Start(path);
                     }
                     else {
                         Process.Start("Explorer.exe", destFolder);
                     }
-
-
-
-
                 }
             }
             catch (Exception exp) {
@@ -49,14 +49,50 @@ namespace THelper {
 
         }
 
-        public static bool GetFile(DirectoryInfo dirInfo, out string path) {
+        public static bool GetFile(DirectoryInfo dirInfo, out string path, out string csprojpath) {
             path = Directory.EnumerateFiles(dirInfo.FullName, "*.sln", SearchOption.AllDirectories).FirstOrDefault();
-
+            csprojpath = Directory.EnumerateFiles(dirInfo.FullName, "*.csproj", SearchOption.AllDirectories).FirstOrDefault();
             if (string.IsNullOrEmpty(path)) {
-                path = Directory.EnumerateFiles(dirInfo.FullName, "*.csproj", SearchOption.AllDirectories).FirstOrDefault();
+                path = csprojpath;
+            }
+            return !string.IsNullOrEmpty(path);
+        }
+
+        private static void FixCsprojSpecificVersion(string cspath) {
+            var flPath = cspath;
+            StreamReader sr = new StreamReader(flPath);
+            XmlTextReader reader = new XmlTextReader(flPath);
+            sr.Close();
+            // XStreamingElement xsr=new XStreamingElement()
+            XElement xlroot = XElement.Load(reader);
+            sr.Close();
+            reader.Close();
+
+            var elements = xlroot.Elements();
+            var references = elements.Where(x => x.Name.LocalName == "ItemGroup" && x.Elements().First().Name.LocalName == "Reference");
+            var dxlibraries = references.Elements().Where(x => x.Attribute("Include").Value.Contains("DevExpress"));
+
+            foreach (XElement dxlib in dxlibraries) {
+
+                var v = dxlib.Value;
+                var v1 = dxlib.NodeType;
+                var v2 = dxlib.Name;
+                var v3 = dxlib.Elements();
+                var v4 = dxlib.Element(XName.Get("SpecificVersion", dxlib.Name.Namespace.NamespaceName));
+                dxlib.SetElementValue(XName.Get("SpecificVersion", dxlib.Name.Namespace.NamespaceName), false);
             }
 
-            return !string.IsNullOrEmpty(path);
+            var currentVersionFull = dxlibraries.First().Attribute("Include").Value;
+            //var vIndex = currentVersionFull.IndexOf("Version=");
+            //var currentVersion = currentVersionFull.Substring(vIndex + 8, 9);
+
+            var st = currentVersionFull.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)[1];
+            var currentVersion = st.Replace("Version=", "");
+
+            string resultString = xlroot.ToString();
+            StreamWriter sw = new StreamWriter(flPath, false);
+            sw.Write(resultString);
+            sw.Close();
         }
     }
 }
