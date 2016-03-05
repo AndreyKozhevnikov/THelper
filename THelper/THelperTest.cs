f028bccde9eda87a54375e49d61293e4fbfb2eac
Andrey K (home) <freazeek@mail.ru>
2016-03-05 21:25:58 +0300

    Add tests

---
+++ THelper/THelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace THelper {
+    [TestFixture]
+    public class THelperTest {
+        [Test]
+        [TestCase]
+        public void GetVersionFromContainingStringTest() {
+            ProjectUpgrader upgr = new ProjectUpgrader(null, null, false);
+           string  st=@"Include=""DevExpress.Data.v15.1, Version=15.1.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a, processorArchitecture=MSIL""";
+           Version v = upgr.GetVersionFromContainingString(st);
+           Assert.AreEqual(v.Major, 151,"major");
+           Assert.AreEqual(v.Minor, 5,"minor");
+        
+        }
+    }
+}
