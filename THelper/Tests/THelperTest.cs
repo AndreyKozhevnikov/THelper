1780ccf63b16b03e2d911d4bc148fb5a1911e356
Andrey K (home) <freazeek@mail.ru>
2016-03-06 09:18:24 +0300

    Test2

--- THelper/Tests/THelperTest.cs
+++ THelper/Tests/THelperTest.cs
@@ -9,7 +9,6 @@ namespace THelper {
     [TestFixture]
     public class THelperTest {
         [Test]
-        [TestCase]
         public void GetVersionFromContainingStringTest() {
             ProjectUpgrader upgr = new ProjectUpgrader(null, null, false);
            string  st=@"Include=""DevExpress.Data.v15.1, Version=15.1.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a, processorArchitecture=MSIL""";
@@ -17,6 +16,11 @@ namespace THelper {
            Assert.AreEqual(v.Major, 151,"major");
            Assert.AreEqual(v.Minor, 5,"minor");
         
+
+            string st2 =@"Include=""DevExpress.Data.v15.1""";
+            Version v2 = upgr.GetVersionFromContainingString(st2);
+            Assert.AreEqual(v2.Major, 151, "major2");
+            Assert.AreEqual(v2.Minor, 0, "minor2");
         }
     }
 }
